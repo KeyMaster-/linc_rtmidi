83cba8e3471c0420933ebf78a25bee604072b5df
Tilman Schmidt <AccessKeymaster@gmx.net>
2015-10-23 18:36:30 +0100

    Changed the queue midi input example to just run for 5 seconds, further tests descriptions.
    Change in qmidiin since there’s apparently no entirely non-blocking way
    to get cli input (even getChar waits for the next char to be typed)

--- tests/qmidiin/Main.hx
+++ tests/qmidiin/Main.hx
@@ -16,38 +16,27 @@ class Main {
         }
         midiin.ignoreTypes(false, false, false);
 
-        Sys.println('Reading MIDI input... press <enter> to quit.');
+        Sys.println('Reading MIDI input for 5 seconds...');
         var stamp:Float;
         var message = haxe.io.Bytes.alloc(1);
 
-        while(true) {
+        var t:Float = haxe.Timer.stamp();
+        while(haxe.Timer.stamp() - t < 5) { // run for 5 seconds
+
             var stamp = midiin.getMessage(message.getData());
             var msgLength = message.getData().length;
             for(i in 0...msgLength){
-                Sys.print("Byte $i = ${message.get(i)}, ");
+                Sys.print('Byte $i = ${message.get(i)}, ');
             }
             if(msgLength > 0) {
                 Sys.print('stamp = $stamp\n');
             }
-            var response = String.fromCharCode(Sys.getChar(true));
-            if(response == '\r' || response == '\n') {
-                break;
-            }
+
             Sys.sleep(0.01);
         }
 
         midiin.destroy();
     } //main
-
-    static function callback(delta:Float, data:haxe.io.BytesData, userData:Dynamic) {
-        var message = haxe.io.Bytes.ofData(data);
-        for(i in 0...message.length) {
-            Sys.print('Byte $i = ${message.get(i)}, ');
-        }
-        if(message.length > 0) {
-            Sys.print('stamp = $delta\n');
-        }
-    }
 
     static function chooseMidiPort(rtmidi:RtMidi):Bool {
         Sys.print('Would you like to open a virtual input port? [y/n] ');
